4b5d9c3a1b4b71914e43bd8d911430060fb46ca1
Kabir <kabirdev.777@gmail.com>
2025-01-31 22:55:21 +0500

    fixed result image aspect ratio in canvas

--- client/src/components/home/ResultImg.jsx
+++ client/src/components/home/ResultImg.jsx
@@ -3,7 +3,6 @@ import useBgRemover from './../../hooks/useBgRemover';
 
 const ResultImg = ({ item, canvasRef }) => {
     const { showOriginal, color } = useBgRemover();
-    console.log(canvasRef)
     useEffect(() => {
         if (item.result && !showOriginal || item.submitted) {
             const canvas = canvasRef.current;
@@ -11,23 +10,12 @@ const ResultImg = ({ item, canvasRef }) => {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.fillStyle = color;
             ctx.fillRect(0, 0, canvas.width, canvas.height);
+
             const img = new Image();
             img.crossOrigin = "anonymous";
-            img.style.maxWidth = '100%';
-            img.style.maxHeight = '100%';
             img.onload = () => {
-                const aspectRatio = img.width / img.height;
-                let drawWidth = canvas.width;
-                let drawHeight = canvas.width / aspectRatio;
-
-                if (drawHeight > canvas.height) {
-                    drawHeight = canvas.height;
-                    drawWidth = canvas.height * aspectRatio;
-                }
-                const x = (canvas.width - drawWidth) / 2;
-                const y = (canvas.height - drawHeight) / 2;
-                ctx.drawImage(img, x, y, drawWidth, drawHeight);
-            }
+                ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            };
             img.src = item.result;
         }
     }, [item, item.submitted, color]);
